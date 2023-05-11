f8dc85515af0978ca3a893d77faa8a2aa5b46d61
root <AdrianWolfP>
2023-05-10 20:53:02 -0700

    fixed file 2 and 3

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -4,16 +4,4 @@ if __name__ == "__main__":
     sum = 0
     for i in range(1, len(argv)):
         sum += int(argv[i])
-    print("{:d}".format(sum))#!/usr/bin/python3
-if __name__ == "__main__":
-    from sys import argv
-    sum = 0
-    for i in range(1, len(argv)):
-        sum += int(argv[i])
-    print("{:d}".format(sum))#!/usr/bin/python3
-if __name__ == "__main__":
-    from sys import argv
-    sum = 0
-    for i in range(1, len(argv)):
-        sum += int(argv[i])
     print("{:d}".format(sum))
