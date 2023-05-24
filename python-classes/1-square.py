067f7ca6668da654248ef03da33bfdcc4e72ee81
Wolfgang Pendergass <119448680+AdrianWolfP@users.noreply.github.com>
2023-05-24 01:14:45 +0000

    f

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Module that creates a class, square"""
+"""Creates an empty class of a square"""
 
 
 class Square:
-""""Is an empty class"""
+    """"Is an empty class of square"""
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("Size must be an integer")
