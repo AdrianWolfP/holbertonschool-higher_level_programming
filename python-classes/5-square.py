a872b8dce0cff411b7a4d84d9b86cd10361caf9a
Wolfgang Pendergass <119448680+AdrianWolfP@users.noreply.github.com>
2023-05-24 02:22:13 +0000

    e

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -11,11 +11,11 @@ class Square:
     def area(self):
         """Area of square"""
         return self.__size ** 2
-    
+
     @property
     def size(self):
         return self.__size
-    
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
@@ -30,4 +30,3 @@ class Square:
             else:
                 for i in range(self.__size):
                     print("#" * self.__size)
-                    
