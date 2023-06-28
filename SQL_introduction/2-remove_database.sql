ce79e1b6fe3221ad6f6608bb9ae3d351140fd484
Wolfgang Pendergass <119448680+AdrianWolfP@users.noreply.github.com>
2023-06-28 11:47:31 -0500

    fixed code in file 7

--- SQL_introduction/2-remove_database.sql
+++ SQL_introduction/2-remove_database.sql
@@ -1,4 +1,4 @@
---SQL script that deletes the database hbtn_0c_0 in your MySQL server.
+-- SQL script that deletes the database hbtn_0c_0 in your MySQL server.
 -- If the datebase_0c_0 doesn't exist, your script should not fail.
 -- You are not allowed to use the SELECT or SHOW statements.
 
